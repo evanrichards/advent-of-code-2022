c1457836ed4b6612ef5f6ebedf468d7b913e32ab
Evan Richards <tevanrichards@gmail.com>
2022-11-30 22:19:31 -0800

    flake8

--- python/aoc/main.py
+++ python/aoc/main.py
@@ -5,12 +5,8 @@ from colorama import Fore, Style
 
 
 @click.command()
-@click.option(
-    "--day", type=click.IntRange(1, 25), default=1, help="Day of the month"
-)
-@click.option(
-    "--part", type=click.IntRange(1, 2), default=1, help="Part of the day"
-)
+@click.option("--day", type=click.IntRange(1, 25), default=1, help="Day of the month")
+@click.option("--part", type=click.IntRange(1, 2), default=1, help="Part of the day")
 @click.option(
     "--input",
     type=click.Choice(["example", "personal"]),
