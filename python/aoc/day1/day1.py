a36732e719372716b23aa946625e3d6adc7f3ad1
Evan Richards <tevanrichards@gmail.com>
2022-11-30 22:11:43 -0800

    Colors

--- python/aoc/day1/day1.py
+++ python/aoc/day1/day1.py
@@ -10,12 +10,10 @@ def main(part, input):
     per_elf = raw_data.split("\n\n")
     per_elf_totals = map(parse_elf_total, per_elf)
     if part == 1:
-        print(max(per_elf_totals))
-        return
+        return max(per_elf_totals)
     if part == 2:
         most_elf = sorted(per_elf_totals, reverse=True)
-        print(sum(most_elf[:3]))
-        return
+        return sum(most_elf[:3])
 
 
 def parse_elf_total(elf):
--- python/aoc/main.py
+++ python/aoc/main.py
@@ -1,5 +1,7 @@
 import click
+import pyperclip
 import day1.day1 as day1
+from colorama import Fore, Style
 
 
 @click.command()
@@ -16,9 +18,15 @@ import day1.day1 as day1
     help="Input file",
 )
 def main(day, part, input):
-    """Run the solution for the given day and part."""
+    click.echo(f"Day {day}, part {part}, input {input}")
     if day == 1:
-        day1.main(part, input)
+        result = day1.main(part, input)
+    else:
+        raise NotImplementedError(f"Day {day} not implemented yet")
+    pyperclip.copy(result)
+    click.echo(
+        f'{Fore.GREEN}Result{Style.RESET_ALL}: {Style.DIM}"{Style.RESET_ALL}{ result }{Style.DIM}" (Copied to clipboard){Style.RESET_ALL}'
+    )
 
 
 if __name__ == "__main__":
