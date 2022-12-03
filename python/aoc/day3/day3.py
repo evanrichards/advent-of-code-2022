773757c6edaa368575c7a9392d3dc54e585d6815
Evan Richards <tevanrichards@gmail.com>
2022-12-02 21:30:09 -0800

    Day 3 rough pass

--- python/aoc/day3/day3.py
+++ python/aoc/day3/day3.py
@@ -6,5 +6,39 @@ def read_day_input(input: str) -> str:
 
 
 def main(part: int, input: str) -> int:
-    _ = read_day_input(input)
-    raise NotImplementedError(f"Day 3, part {part} not implemented yet")
+    raw = read_day_input(input)
+    lines = raw.splitlines()
+
+    priorities = [
+        [letter_to_priority(letter) for letter in list(line)] for line in lines
+    ]
+    if part == 1:
+        # split into two lists by halving the list
+        splits = map(split_list, priorities)
+    elif part == 2:
+        # group list by every 3 lines
+        splits = [priorities[i : i + 3] for i in range(0, len(priorities), 3)]
+    return sum(map(find_duplicates, splits))
+
+
+def find_duplicates(lines: list[list[int]]) -> None:
+    for line in lines:
+        line.sort()
+    first = set(lines[0])
+    rest = lines[1:]
+    intersection = first
+    for line in rest:
+        intersection = intersection.intersection(line)
+    return intersection.pop()
+
+
+def split_list(priorities: list[int]) -> tuple[list[int], list[int]]:
+    half = len(priorities) // 2
+    return priorities[:half], priorities[half:]
+
+
+def letter_to_priority(letter: str) -> int:
+    if letter <= "Z":
+        return ord(letter) - ord("A") + 27
+    else:
+        return ord(letter) - ord("a") + 1
