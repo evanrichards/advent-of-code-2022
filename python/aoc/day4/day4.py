5c23841f1cee11de7d8bfda11b9a7c2683fa1719
Evan Richards <tevanrichards@gmail.com>
2022-12-03 21:18:06 -0800

    Day 4 cleanup

--- python/aoc/day4/day4.py
+++ python/aoc/day4/day4.py
@@ -1,5 +1,4 @@
 from read_input_common import read_input_common
-from functools import reduce
 
 
 def read_day_input(input: str) -> str:
@@ -12,20 +11,15 @@ def main(part: int, input: str) -> int:
     range_pairs = map(parse_ranges, lines)
     if part == 1:
         return len(list(filter(fully_contained, range_pairs)))
-    if part == 2:
-        return len(list(filter(overlap, range_pairs)))
-    raise ValueError("Part must be 1 or 2")
+    return len(list(filter(overlap, range_pairs)))
 
 
 def parse_ranges(line: str) -> tuple:
-    print(line)
-    one, two = line.split(",")
-    return parse_range(one), parse_range(two)
+    return list(map(parse_range, line.split(",")))
 
 
 def parse_range(range: str) -> tuple:
-    beginning, end = range.split("-")
-    return int(beginning), int(end)
+    return list(map(int, range.split("-")))
 
 
 def fully_contained(pair: tuple) -> bool:
