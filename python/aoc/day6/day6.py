12a20bb3a48d396dec171db7bb2e58e2d883475e
Evan Richards <tevanrichards@gmail.com>
2022-12-05 21:46:35 -0800

    Day 6 single pass solution

--- python/aoc/day6/day6.py
+++ python/aoc/day6/day6.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from read_input_common import read_input_common
 
 
@@ -8,19 +10,25 @@ def read_day_input(input: str) -> str:
 def main(part: int, input: str) -> int:
     raw = list(read_day_input(input))
     header_len = 4 if part == 1 else 14
-    buffer = [None for _ in range(header_len)]
+    buffer = []
+    letters_to_pop = []
+    active = defaultdict(lambda: 0)
     for i, letter in enumerate(raw):
-        buffer[i % header_len] = letter
-        if i > header_len - 1:
-            good = check(buffer)
-            if good:
-                return i + 1
+        current_count = active[letter]
+        current_count += 1
+        active[letter] = current_count
+        if current_count > 1:
+            letters_to_pop.append(letter)
+        buffer.append(letter)
+        if len(buffer) <= header_len:
+            continue
+        popped_letter = buffer.pop(0)
+        popped_count = active[popped_letter]
+        popped_count -= 1
+        active[popped_letter] = popped_count
+        while len(letters_to_pop) > 0 and active[letters_to_pop[0]] < 2:
+            letters_to_pop.pop(0)
+        if len(letters_to_pop) == 0:
+            return i + 1
+    raise "No solution"
 
-
-def check(letter_list) -> bool:
-    existing = {}
-    for letter in letter_list:
-        if letter in existing:
-            return False
-        existing[letter] = True
-    return True
